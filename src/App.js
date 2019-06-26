d979d4c82e5dddf38bbf042fc0b5aedae32005eb
Bankgrammer <codeconstable@gmail.com>
2019-06-26 16:49:57 +0200

    Intial Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Die from './Die'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Die face="five" />
+      <Die face="six" />
+      <Die face="three" />
+      <Die face="two" />
+
+    </div>
+  );
+}
+
+export default App;
