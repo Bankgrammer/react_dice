dd796e619e9ce41f1ccb596be4295b078ac8983f
Bankgrammer <codeconstable@gmail.com>
2019-07-01 15:00:51 +0200

    Styling React Dice

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import Die from './Die'
-import './App.css';
+import React, { Component } from "react";
+import RollDice from "./RollDice";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Die face="five" />
-      <Die face="six" />
-      <Die face="three" />
-      <Die face="two" />
-
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <RollDice />
+      </div>
+    );
+  }
 }
 
 export default App;
